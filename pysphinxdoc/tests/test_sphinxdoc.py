5f5ed8d4c71256fb68f3ad60cbbc43984faa2315
Antoine Grigis <antoine.grigis@cea.fr>
2016-04-13 15:15:20 +0200

    pysphinxdoc/tests/test_sphinxdoc: add class test to increase coverage.

--- pysphinxdoc/tests/test_sphinxdoc.py
+++ pysphinxdoc/tests/test_sphinxdoc.py
@@ -10,22 +10,46 @@
 import unittest
 import os
 import subprocess
+from setuptools import find_packages
+import shutil
 
 # Pysphinxdoc import
 import pysphinxdoc
+from pysphinxdoc.docgen import DocHelperWriter
 
 
 class SphinxDoc(unittest.TestCase):
     """ Test the documentation generation script.
     """
-    def test_normal_execution(self):
-        """ Test the normal behaviour of the function.
+    def test_command_execution(self):
+        """ Test the normal behaviour of the command.
         """
         moduledir = os.path.dirname(pysphinxdoc.__path__[0])
         docdir = os.path.join(moduledir, "doc")
+        generateddir = os.path.join(docdir, "source", "generated")
+        if os.path.isdir(generateddir):
+            shutil.rmtree(generateddir)
         cmd = ["sphinxdoc", "-p",  moduledir, "-n", "pysphinxdoc", "-o",
                docdir, "-v", "2"]
         subprocess.check_call(cmd)
+
+    def test_class_execution(self):
+        """ Test the normal behaviour of the class.
+        """
+        moduledir = os.path.dirname(pysphinxdoc.__path__[0])
+        docdir = os.path.join(moduledir, "doc")
+        generateddir = os.path.join(docdir, "source", "generated")
+        if os.path.isdir(generateddir):
+            shutil.rmtree(generateddir)
+        module_names = find_packages(moduledir)
+        docwriter = DocHelperWriter(moduledir, docdir, module_names,
+                                    "pysphinxdoc", verbose=2)
+        docwriter.write_sphinx_config()
+        docwriter.write_layout()
+        docwriter.write_index()
+        docwriter.write_installation()
+        docwriter.write_documentation_index()
+        docwriter.write_api_docs()
 
 
 if __name__ == "__main__":

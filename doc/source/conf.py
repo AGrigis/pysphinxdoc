89542d22bc66bd0676b4930e3254dd1244cf7d0a
Antoine Grigis <antoine.grigis@cea.fr>
2019-10-29 11:59:06 +0100

    fix doc generation.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -183,9 +183,7 @@ html_short_title = "pysphinxdoc"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default
-
-.css" will overwrite the builtin "default.css".
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = [
     "_static"]
 
